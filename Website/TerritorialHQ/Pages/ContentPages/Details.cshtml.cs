e472e4bc20b3bf1d09899b1a3465f30aec25754f
GDuath <duath83@gmx.de>
2023-05-17 12:35:10 +0200

    Added tailwindcss to frontend part of the website

--- Website/TerritorialHQ/Pages/ContentPages/Details.cshtml.cs
+++ Website/TerritorialHQ/Pages/ContentPages/Details.cshtml.cs
@@ -21,16 +21,19 @@ namespace TerritorialHQ.Pages.ContentPages
 
         public async Task<IActionResult> OnGet()
         {
+            // Read the URL to determine which content page the user wanted to see
             var pathArr = Request.Path.Value.TrimEnd('/').Split("/");
             var slug = pathArr[pathArr.Length - 1];
 
             if (slug == null || slug.Length == 0)
                 return NotFound();
 
+            // See if the slug belongs to an actual navigation entry
             NavEntry = await _navigationEntryService.GetBySlugAsync(slug);
             if (NavEntry == null) 
                 return NotFound();
 
+            // Get the content page
             if (NavEntry.ContentPageId != null)
                 ContentPage = await _contentPageService.FindAsync(NavEntry.ContentPageId);
 
